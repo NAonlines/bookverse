bcc314ac18c5a1716c5d0461cca3687a6c895e65
NAonlines <tenshiyami98@users.noreply.github.com>
2025-05-31 21:17:23 +0700

    Bookverse V - 1.0.1

--- src/app/(book)/test/page.js
+++
@@ -1,9 +0,0 @@
-import PublicLayout from "@/app/(book)/layout";
-import Navigation from "@/components/NavigationBook";
-import React from "react";
-
-function page() {
-   return <div>Test</div>;
-}
-
-export default page;
